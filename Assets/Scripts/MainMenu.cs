a732a17a309b2ab34e1f56379759f0163e8f019b
5yph <65436823+5yph@users.noreply.github.com>
2021-05-25 15:53:10 -0600

    Update main menu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,4 +10,16 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         // will load next scene sequentially according to build order
     }
+
+    /* Quit functionality is irrelevant for WebGL (cannot quit browser-based games)
+    However, it is included here in case you want to publish to other platforms
+
+    */
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    //
 }
