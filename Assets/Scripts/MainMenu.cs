44981143241d65f6991c00d21915cee27c93867f
5yph <65436823+5yph@users.noreply.github.com>
2021-05-25 15:37:45 -0600

    Updates main menu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,5 +8,6 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // will load next scene sequentially according to build order
     }
 }
