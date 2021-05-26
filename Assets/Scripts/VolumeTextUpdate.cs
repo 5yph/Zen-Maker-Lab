84f3570cf25ab56d2aceede7f00b4b080900908d
5yph <65436823+5yph@users.noreply.github.com>
2021-05-25 23:35:05 -0600

    Initial commit

---
+++ Assets/Scripts/VolumeTextUpdate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VolumeTextUpdate : MonoBehaviour
+{
+    public TextMeshProUGUI volText; // new text object
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volText = this.GetComponent<TextMeshProUGUI>(); // set volText the TMP component
+    }
+
+    public void UpdateText(float value) // 'value' is automatically passed from slider object
+    {
+        volText.text = "Volume: " + value + "%"; // change volume text
+    }
+
+}
