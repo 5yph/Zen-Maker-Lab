2bfc8c3553c3637f802c2df268fb12095edf192e
5yph <65436823+5yph@users.noreply.github.com>
2021-06-01 20:30:03 -0600

    Added new template

---
+++ Assets/Template Scripts/PlayerMovement Template.cs
@@ -0,0 +1,216 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// THIS IS THE TEMPLATE FOR THE FINAL VERSION OF THE PLAYER MOVEMENT SCRIPT
+// Do not attempt unless you have already completed the previous templates
+public class PlayerMovementTemplate : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private CapsuleCollider2D hitbox;
+    public LayerMask groundLayer; // what is considered ground
+    public LayerMask ceilingLayer; // what is considered ceiling
+
+    [Header("Speeds")]
+    [SerializeField] private float movespeed;
+    [SerializeField] private float jumpspeed;
+    [SerializeField] private float short_jumpspeed; // jump speed if jump button only tapped
+    [SerializeField] private float crouchspeed; // speed of character when crouching
+
+    [Space]
+
+    [Header("Ground Check")]
+    [SerializeField] private Vector2 grounded_offset; // where should our circle detecting ground be located?
+    [SerializeField] private float grounded_radius = 0.25f; // how big should our ground check circle be
+
+    [Space]
+
+    [Header("Crouch")]
+
+    [SerializeField] private float crouch_height_modifier; // a decimal value from 0.5-0.99 that tells you 
+    // what percentage of original height the crouched height will be. DO NOT SET LOWER THAN 0.5
+    [SerializeField] private Vector2 ceiling_check_offset; // where should our circle detecting ground be located?
+    [SerializeField] private float ceiling_check_radius = 0.25f; // how big should our ground check circle be
+
+    private Vector2 direction; // movement direction
+    private Vector2 hitbox_size_original; // width and height of capsule collider
+    private Vector2 hitbox_offset_original; // original hitbox offset
+
+    private int jump_count = 2; // how many times can we jump
+    private bool grounded = false; // is the character touching ground?
+    private bool try_jump = false; // is the player trying to jump?
+    private bool jump_cancelled = false; // did player cancel jump?
+
+    private bool crouch = false; // is the player crouching
+    private bool try_uncrouch = false; // is the player trying to stand up
+    private bool ceiling_check = false; // is there a ceiling above us?
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        hitbox = GetComponent<CapsuleCollider2D>();
+        hitbox_size_original = hitbox.size;
+        hitbox_offset_original = hitbox.offset;
+        // get reference to components, allows us to use built-in functions
+    }
+
+    void Update()
+    {   // detect input in Update() so we don't miss any inputs
+
+        float x_component = Input.GetAxisRaw("Horizontal");
+
+        if (!crouch)
+        {
+            // uncrouched movespeed
+            direction = new Vector2(x_component * movespeed, rb.velocity.y);
+        }
+        else
+        {
+            /* IMPLEMENT a new way to deal with movement speed while crouched.
+        Before we had a constant movespeed. Now, after checking that the player is crouched,
+        our new movespeed should be the crouchspeed instead. 
+        */ 
+        }
+
+        grounded = IsGrounded();
+        
+        // ADD A CEILING CHECK, just like the ground check, with the UnderCeiling() function
+
+        if (grounded)
+        {
+            jump_count = 2;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            // user presses jump
+            if (grounded)
+            {
+                try_jump = true;
+                jump_count--;
+            }
+            else if (!grounded && (jump_count == 2))
+            {
+                // we are in the air, but we still have 2 jumps
+                // i.e. we fell off a cliff
+                try_jump = true;
+                jump_count = 0; // only jump once
+            }
+            else if (jump_count > 0)
+            {
+                // Player in air, but can still jump 
+                try_jump = true;
+                jump_count--;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump") && !grounded)
+        {
+            // if the player cancelled the jump in mid-air, don't jump as high
+            jump_cancelled = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && grounded) // replace with key of choice
+        {
+            // user presses crouch (the down arrow)
+
+            // IF USER PRESSES CROUCH, we should change the values of vars 'crouch' and 'try_uncrouch'.
+            // Implement this. ('crouch' calls the Crouch() function, 'try_uncrouch' calls UnCrouch() if no ceiling).
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+
+            // user stops holding crouch
+            // UPDATE values for 'crouch' and 'try_uncrouch'. We want to try to uncrouch here.
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        Move(direction);
+
+        if (try_jump)
+        {
+            Jump();
+        }
+
+        if (jump_cancelled)
+        {
+            JumpCancel();
+        }
+
+        if (crouch)
+        {
+            Crouch();
+        }
+
+        if (try_uncrouch)
+        {
+            /* Before we can allow our character to uncrouch, we need to make sure that
+            there is no overhead ceiling, otherwise we could get stuck.
+
+            IMPLEMENT this. Remember UnCrouch() and ceiling_check.
+            */
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle((Vector2)transform.position + grounded_offset, grounded_radius, groundLayer);
+    }
+
+    private bool UnderCeiling()
+    {
+        return Physics2D.OverlapCircle((Vector2)transform.position + ceiling_check_offset, ceiling_check_radius, ceilingLayer);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        rb.velocity = direction;
+        // Create a velocity in a specific direction
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(0, jumpspeed);
+        try_jump = false;
+    }
+
+    private void JumpCancel()
+    {
+        if (rb.velocity.y > short_jumpspeed)
+        {
+            // change characters velocity to short jump velocity
+            rb.velocity = new Vector2(rb.velocity.x, short_jumpspeed);
+        }
+        jump_cancelled = false;
+    }
+
+    private void Crouch()
+    {
+        hitbox.size = new Vector2(hitbox_size_original.x, hitbox_size_original.y * crouch_height_modifier);
+        // when crouching, halve the height of hitbox, but don't change width
+        // we also need to move this hitbox to the base of the capsule
+
+        hitbox.offset = new Vector2(hitbox_offset_original.x, -(hitbox_size_original.y - hitbox.size.y) / 2);
+    }
+
+    private void UnCrouch()
+    {
+        // no ceiling above us, safe to stand up
+        hitbox.size = hitbox_size_original;
+        hitbox.offset = hitbox_offset_original;
+        // go back to original hitbox size and offset
+        crouch = false;
+        try_uncrouch = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw the circle that detects where ground + ceiling is
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere((Vector2)transform.position + grounded_offset, grounded_radius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + ceiling_check_offset, ceiling_check_radius);
+
+    }
+}
