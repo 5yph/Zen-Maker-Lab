6a17c295d9ad4c48920cab2cec9a5145615bc9a4
5yph <65436823+5yph@users.noreply.github.com>
2021-06-07 13:49:51 -0600

    Initial commit

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -7,17 +7,25 @@ public class BasicEnemy : MonoBehaviour
     // Script for enemy that just walks forward
 
     [SerializeField] private float movespeed = 5f;
+    [SerializeField] private bool moves_right = false; // does the enemy move right? (default is left)
+
+    private Rigidbody2D enemy;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemy = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (moves_right)
+        {
+            enemy.velocity = new Vector2(movespeed, enemy.velocity.y);
+        } else
+        {
+            enemy.velocity = new Vector2(-movespeed, enemy.velocity.y);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,7 +34,16 @@ public class BasicEnemy : MonoBehaviour
         {
             Destroy(gameObject);
             // if enemy gets hit with player projectile, it should die.
+            Destroy(collision.gameObject); 
+            // the projectile is also destroyed
         }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            // if enemy hits a player, we deal damage to them
+            collision.gameObject.GetComponent<Damage>().DealDamage(1);
+        }
+
     }
 
 }
