1b153832d6be0a35097d3dfedf507ab94d1771da
5yph <65436823+5yph@users.noreply.github.com>
2021-06-07 17:49:28 -0600

    Initial commit

---
+++ Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    [SerializeField] private float movespeed = 3f;
+    private Vector2 direction;
+    private bool to_point_2 = true; // are we going to point 2
+
+    public Transform point1; // first waypoint
+    public Transform point2; // second waypoint
+    private Rigidbody2D enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<Rigidbody2D>();
+        enemy.gravityScale = 0; // no gravity for flying enemies
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (to_point_2)
+        {
+            direction = point2.position - transform.position;
+            // we are going to point 2
+        } else
+        {
+            direction = point1.position - transform.position;
+            // we are going to point 1
+        }
+
+        if (Vector2.Distance(point2.position, transform.position) < 0.1f && to_point_2 == true)
+        {
+            // if we just arrived at point2 at a close enough distance
+            to_point_2 = false;
+        } else if (Vector2.Distance(point1.position, transform.position) < 0.1f && to_point_2 == false)
+        {
+            // if we just arrived at point1
+            to_point_2 = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        direction.Normalize(); // only care about the direction
+        enemy.velocity = direction * movespeed;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw a line between the 2 waypoints
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(point1.position, point2.position);
+    }
+
+}
