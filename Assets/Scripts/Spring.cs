1664e7fceff58f6df588ed51ad8966e388c4ae0a
5yph <65436823+5yph@users.noreply.github.com>
2021-06-08 16:21:31 -0600

    Vertical functional

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -4,43 +4,30 @@ using UnityEngine;
 
 public class Spring : MonoBehaviour
 {
-    [SerializeField] private float spring_force = 5f;
+    [SerializeField] private float spring_force = 5f; // force of spring
+    [SerializeField] private bool up = true; // direction of spring
 
+    private Vector2 direction;
     private Rigidbody2D obj; // Object that hits our spring 
-    public Vector2 direction; // direction of spring force
 
     // Start is called before the first frame update
     void Start()
     {
-        direction.Normalize();
-        Debug.Log(direction * spring_force);
+        if (up)
+        {
+            direction = new Vector2(0, 1);
+        } else
+        {
+            direction = new Vector2(0, -1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         obj = collision.gameObject.GetComponent<Rigidbody2D>();
         obj.velocity = new Vector2(0, 0); // freeze the player before they bounce, more cartoony
         obj.velocity = direction * spring_force;
-
-        if (direction.x != 0)
-        {
-            // if there is some horizontal movement, need to adjust spring 
-            // this is because our movement script directly manipulates horizontal velocity
-
-        }
-
-        Debug.Log(direction * spring_force);
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawRay(transform.position, direction);
-    }
 }
