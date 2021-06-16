e67d43e72103b8460627232e87d3a382d175753d
5yph <65436823+5yph@users.noreply.github.com>
2021-06-16 12:29:36 -0600

    Update animator

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     PlayerMovement player;
+    PlayerProjectileTrigger shooter;
     Animator animator;
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         player = GetComponent<PlayerMovement>();
+        shooter = GetComponent<PlayerProjectileTrigger>();
     }
 
     // Update is called once per frame
@@ -24,6 +28,43 @@ public class PlayerAnimator : MonoBehaviour
         } else
         {
             animator.SetBool("Moving", true);
+        }
+
+        if (rb.velocity.y > 0.001)
+        {
+            // player is shooting up into the air
+            animator.SetBool("Falling", false);
+            animator.SetBool("Jumping", true);
+        } else if (rb.velocity.y < -0.001)
+        {
+            // player is falling
+            animator.SetBool("Jumping", false);
+            animator.SetBool("Falling", true);
+        } else
+        {
+            animator.SetBool("Falling", false);
+            animator.SetBool("Jumping", false);
+        }
+
+        if (player.crouch)
+        {
+            // player is crouching 
+            animator.SetBool("Crouching", true);
+        }
+
+        if (player.try_uncrouch && !player.ceiling_check)
+        {
+            // if player can successfully uncrouch
+            animator.SetBool("Crouching", false);
+        }
+
+        if (shooter.shooting)
+        {
+            // if player is shooting
+            animator.SetBool("Shooting", true);
+        } else
+        {
+            animator.SetBool("Shooting", false);
         }
     }
 }
