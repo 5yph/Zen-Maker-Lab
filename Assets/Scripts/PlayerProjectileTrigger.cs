3e67fe4426006a0336ca60afa7f6dd7aae26acce
5yph <65436823+5yph@users.noreply.github.com>
2021-06-16 12:29:07 -0600

    Create new public shooting variable (need to amend)

--- Assets/Scripts/PlayerProjectileTrigger.cs
+++ Assets/Scripts/PlayerProjectileTrigger.cs
@@ -9,18 +9,27 @@ public class PlayerProjectileTrigger : MonoBehaviour
     
     [SerializeField] private float cooldown; // fire rate of projectile, in seconds
     private float next_time_can_fire; // when we can fire the next projectile
+    public bool shooting = false; // are we shooting (for animator purposes)
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButton("Fire1")) // Get button allows you to hold the button
         {
+            shooting = true;
             if (next_time_can_fire <= Time.time)
             {
                 // if user presses the fire button
                 Instantiate(projectile, spawn.position, spawn.rotation);
                 next_time_can_fire = Time.time + cooldown; // next time we can fire is current time + cooldown
-            }
+               // shooting = true;
+            } // else
+            // {
+               // shooting = false;
+           // }
+        } else
+        {
+            shooting = false;
         }
 
     }
