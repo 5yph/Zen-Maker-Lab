1bcd21b969acbe05be5ee5d10d313433ac147f38
5yph <65436823+5yph@users.noreply.github.com>
2021-06-18 17:19:38 -0600

    Overhaul damage

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
-    public float life = 1;
+    public int lives = 1;
+    private int current_life;
+
     public Transform Spawn; // where we respawn
+    private Rigidbody2D rb;
+
     [HideInInspector] public bool dead = false; // are we dead?
+    [HideInInspector] public bool respawning = false; // are we respawning?
+    private float death_time; // how long it takes for us to die after hitting
+    private float respawn_time; // how long it takes for us to respawn after dying
+
+    private void Start()
+    {
+        current_life = lives;
+        rb = GetComponent<Rigidbody2D>();
+        // get the death time from the animation script
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -26,20 +41,44 @@ public class Damage : MonoBehaviour
 
     public void DealDamage(int damage) // make this public so anything can deal damage to player
     {
-        life = life - damage;
-        if (life <= 0)
+        current_life = current_life - damage;
+        if (current_life <= 0)
         {
-            dead = true;
             Die();
         }
     }
 
     public void Die() // public so anything can cause instant death
     {
-        // Respawn
-        transform.position = Spawn.position;
+        dead = true;
+        // gets the length of time that our death animation will play
+
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        // Freeze all movement of player when we die
+
+        StartCoroutine("Respawn");
+
         // move our character back to spawn position
-        dead = false; // respawned
+    }
+
+    IEnumerator Respawn()
+    {
+        death_time = GetComponent<PlayerAnimator>().death_time;
+        yield return new WaitForSeconds(death_time);
+        // spend some time before respawning (to play death animation)
+
+        dead = false;
+        respawning = true;
+        transform.position = Spawn.position; // Go to respawn point
+        current_life = lives; // reset lives
+
+        respawn_time = GetComponent<PlayerAnimator>().respawn_time;
+        yield return new WaitForSeconds(respawn_time);
+
+        respawning = false;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        // Go back to default constraints (freezing rotation).
+        rb.velocity = Vector2.down; // add some gravity so it doesn't freeze in air
     }
 
 }
